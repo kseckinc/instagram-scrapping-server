df92b0395498012fa03704c16ae40adaa4205662
root <pyphist@outlook.com>
2017-11-14 10:54:55 -0800

    forms.py updated

--- app/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class DirectMessageForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':"Enter username"}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'md-textarea form-control', 'rows': 5, 'placeholder':"Enter Message"}))
