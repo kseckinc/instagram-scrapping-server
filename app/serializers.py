20892b7203dc56beb23aa3a78551022b4f6cd26a
root <pyphist@outlook.com>
2019-03-29 15:59:28 -0700

    serializers.py updated

--- app/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-from .models import Account
-
-class AccountSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Account
-        fields = '__all__'
